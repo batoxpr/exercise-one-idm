352eaadfe5a97ffc60d563adef0af32b00720bb2
Bato Batbileg <batoxpr@10-18-217-20.dynapool.wireless.nyu.edu>
2023-09-05 09:41:41 -0400

    second commit

---
+++ Class note.js
@@ -0,0 +1,16 @@
+// Variables
+// var name0fVariable = ''
+
+const constantVariable = 'VALUE';
+let changeableVariable = 'something that changes';
+
+/* Arrays */
+
+const coolArray = [
+    'string',
+    0,
+];
+
+coolArray[0] = 'another string'';
+// you can still change it this way
+
